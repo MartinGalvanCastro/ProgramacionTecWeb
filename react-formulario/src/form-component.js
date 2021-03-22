f0b4c279845ed2efd71d076862a1cf8a78eccece
Martin Galvan <md.galvan@uniandes.edu.co>
2021-03-21 22:14:51 -0500

    Listo Formulario

--- react-formulario/src/form-component.js
+++ react-formulario/src/form-component.js
@@ -7,22 +7,83 @@ import useSignupForm from "./myFormHook";
 
 /**
  * Función de validación de cada campo
- * @param {*} validation, validaciones de el campo 
- * @returns 
+ * @param {*} validation, validaciones de el campo
  */
-function validationSquema(validation) {
-  return null;
-}
+function validationSquema(field) {
+  let valid = null;
+  let isValid = true;
+  switch (field.type) {
+    case "text":
+      valid = Joi.string();
+      break;
+    case "email":
+      valid = Joi.string().email({ tlds: { allow: false } });
+      break;
+    case "tel":
+      valid = Joi.string();
+      break;
+    case "password":
+      if(field.id==="repeat-password"){
+        valid = Joi.ref("password");
+        isValid=false;
+      }else{
+        valid = Joi.string()
+      }
+      break;
+    default:
+      isValid = false;
+      break;
+  }
 
+  if (isValid) {
+    field.validation.forEach((req) => {
+      switch (req.name) {
+        case "required":
+          valid = valid.required();
+          break;
+        case "minLength":
+          valid = valid.min(req.value);
+          break;
+        case "pattern":
+          valid = valid.regex(new RegExp(req.value));
+          break;
+        case "equal":
+          valid = valid.ref(req.value);
+          break;
+        default:
+          break;
+      }
+
+      valid.error((errors) => {
+        errors.forEach((err) => {
+          switch (err.type) {
+            case "any.empty":
+              err.message = req.badMessage;
+              break;
+            case "any.min":
+              err.message = req.badMessage;
+              break;
+            case "any.regex":
+              err.message = req.badMessage;
+              break;
+            default:
+              err.message = err.type;
+          }
+        });
+      });
+    });
+  }
+
+  return { [field.id]: valid };
+}
 
 /**
  * Funcion para renderizar los campos del formulario
  * @param {*} field, Campo del formulario
- * @param {*} handleInputChange, Función de handle input change 
+ * @param {*} handleInputChange, Función de handle input change
  * @returns Input renderizado
  */
-function FieldComponent(field,handleInputChange) {
-  console.log(field.validation)
+function FieldComponent(field, handleInputChange) {
   return (
     <div className="col-6" key={field.id}>
       <Form.Group controlId={field.id}>
@@ -40,19 +101,24 @@ function FieldComponent(field,handleInputChange) {
 
 /**
  * Función que renderiza un formulario con dos botones
- * @param {*} props, archivo JSON que tiene los datos del formulario 
+ * @param {*} props, archivo JSON que tiene los datos del formulario
  * @returns Formulario renderizado
  */
 function FormComponent(props) {
-  const { handleSubmit, handleInputChange } = useSignupForm();
   const formMetadata = props.props.metadata;
   const formFields = props.props.fields;
+  let validationFields = [...formFields];
+  validationFields = validationFields.map((field) => validationSquema(field));
+  let joiSchema = {}
+  validationFields.forEach((field) => joiSchema = {...joiSchema,...field});
+  joiSchema = Joi.object(joiSchema)
+  const { handleSubmit, handleInputChange, errors } = useSignupForm(joiSchema);
+  console.log(errors)
   return (
-    <Form onSubmit={handleSubmit}
-      autoComplete={formMetadata.autocomplete}>
+    <Form onSubmit={handleSubmit} autoComplete={formMetadata.autocomplete}>
       <Row>
         {formFields.map((field) => {
-          return FieldComponent(field,handleInputChange);
+          return FieldComponent(field, handleInputChange);
         })}
       </Row>
       <Row className="mt-3">
@@ -63,6 +129,13 @@ function FormComponent(props) {
           <Button variant="danger" type="reset">
             Cancelar
           </Button>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col>
+        <p>
+          {errors.toString()}
+        </p>
         </Col>
       </Row>
     </Form>
--- react-formulario/src/myFormHook.js
+++ react-formulario/src/myFormHook.js
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
-const useSignupForm = () => {
-    
-    const [inputs,setInputs] = useState({});
+const useSignupForm = (schema) => {
+  const [inputs, setInputs] = useState({});
+  const [errors, setErrors] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("Form Submited",inputs);
-    };
-
-
-    const handleInputChange = (event) => {
-        setInputs({...inputs,[event.target.name]: event.target.value});
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { error } = validate();
+    if (!error) {
+      console.log("Form submitted!", inputs);
+    } else {
+      console.log("Errors", error);
+      setErrors(error);
     }
+  };
 
+  const handleInputChange = (event) => {
+    setInputs({ ...inputs, [event.target.name]: event.target.value });
+  };
 
-    return {handleSubmit,handleInputChange};
-}
+  const validate = () => {
+    return schema.validate(inputs);
+  };
+
+  return { handleSubmit, handleInputChange, errors };
+};
 
 export default useSignupForm;
